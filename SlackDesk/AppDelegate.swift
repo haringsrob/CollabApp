59120002bafc32219f60f289c0b8fc92a52176e5
Rob Harings <haringsrob@gmail.com>
2018-10-12 21:27:25 +0200

    Initial Commit

---
+++ SlackDesk/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlackDesk
+//
+//  Created by Rob Harings on 12/10/2018.
+//  Copyright © 2018 Rob Harings. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SlackDesk/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SlackDesk
+//
+//  Created by Rob Harings on 12/10/2018.
+//  Copyright © 2018 Rob Harings. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
