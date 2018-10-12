396b6c291c71ccf8b01b1fa008e861dc8e0cbf9f
Rob Harings <haringsrob@gmail.com>
2018-10-13 00:05:12 +0200

    Functioning tests

--- SlackDeskTests/ControllerTests/SettingsControllerTests.swift
+++ SlackDeskTests/ControllerTests/SettingsControllerTests.swift
@@ -13,12 +13,36 @@ class SettingsControllerTests: XCTestCase {
     private let settingsController: SettingsController = SettingsController()
     
     public func testConnectionSettings() -> Void {
-        let settings: Array<String> = ["token-1", "token-2"]
+        UserDefaults.blankDefaultsWhile {
+            XCTAssertEqual([""], self.settingsController.getConnectionTokens())
+            
+            let settings: Array<String> = ["token-1", "token-2"]
+            
+            self.settingsController.setConnectionTokens(connections: settings)
+            XCTAssertEqual(["token-1", "token-2"], self.settingsController.getConnectionTokens())
+            
+            self.settingsController.addConnectionToken(token: "token-3")
+            XCTAssertEqual(["token-1", "token-2", "token-3"], self.settingsController.getConnectionTokens())
+        }
+    }
+}
+
+// Ensures that the defaults are empty when running the tests.
+// @see: http://www.figure.ink/blog/2016/10/15/testing-userdefaults
+extension UserDefaults {
+    static func blankDefaultsWhile(handler:()->Void){
+        let bundle = Bundle.main
+        let defs = UserDefaults.standard
         
-        self.settingsController.setConnectionTokens(connections: settings)
-        XCTAssertEqual(["token-1", "token-2"], self.settingsController.getConnectionTokens())
+        guard let name = bundle.bundleIdentifier else {
+            fatalError("Couldn't find bundle ID.")
+        }
+        let old = defs.persistentDomain(forName: name)
+        defer {
+            defs.setPersistentDomain( old ?? [:], forName: name)
+        }
         
-        self.settingsController.addConnectionToken(token: "token-3")
-        XCTAssertEqual(["token-1", "token-2", "token-3"], self.settingsController.getConnectionTokens())
+        defs.removePersistentDomain(forName: name)
+        handler()
     }
 }
