29060a5513e21ce725a5cb904ca7a2cbcccc551e
Rob Harings <haringsrob@gmail.com>
2018-10-25 20:10:49 +0200

    Cleanup

--- Pods/Target Support Files/CrossroadRegex/CrossroadRegex-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CrossroadRegex : NSObject
-@end
-@implementation PodsDummy_CrossroadRegex
-@end
--- Pods/Target Support Files/Smile/Smile-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Smile : NSObject
-@end
-@implementation PodsDummy_Smile
-@end
